c797a0700eb3182b83009c806355e463878d6a42
Abhiram123662 <abhiramayyagari36@gmail.com>
2026-06-11 06:14:46 +0000

    Sync LeetCode submission Runtime - 3 ms (66.15%), Memory - 19.3 MB (23.44%)

--- 0069-sqrtx/solution.py
+++ 0069-sqrtx/solution.py
@@ -1,9 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x<2:
-            return x
-        a=2
-        while a*a<=x:
-            a+=1
-        return a-1
+        l=1
+        r=x
+        while(l<=r):
+            m=(l+r)//2
+            msq=m*m
+            if msq==x:
+                return m
+            elif msq<x:
+                l=m+1
+            else:
+                r=m-1
+        return r
         
