6d4171d022a8755fda99294fb2696150beaa01c3
Abhiram123662 <abhiramayyagari36@gmail.com>
2026-07-08 05:01:45 +0000

    Sync LeetCode submission Runtime - 21 ms (5.11%), Memory - 19.3 MB (73.06%)

--- 0728-self-dividing-numbers/solution.py
+++ 0728-self-dividing-numbers/solution.py
@@ -1,18 +1,20 @@
 class Solution:
-    def check(self, num):
-        st = str(num)
-        if '0' in st: return False
-        for i in range(len(st)):
-            if num % int(st[i]): return False
-        return True
-
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        ret_arr = []
-        for i in range(left, right+1):
-            if self.check(i):
-                ret_arr.append(i)
-        return ret_arr
- 
-
+        
+        def snum(n):
+            count=0
+            for d in str(n):
+                if int(d)!=0:
+                    if n%(int(d))==0:
+                        count+=1
+            if count==len(str(n)):
+                return True
+            else:
+                return False
+        res=[]
+        for i in range(left,right+1):
+            if snum(i)==True:
+                res.append(i)
+        return res
 
 
